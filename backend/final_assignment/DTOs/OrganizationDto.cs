b8c2af6f703ae6916e41293dd8ace922a327010a
Deshapriya LGAL <227109712+dulanjanaabishek2003-eng@users.noreply.github.com>
2026-07-22 00:33:48 +0530

    feat: add OrganizationDto data transfer object

---
+++ backend/final_assignment/DTOs/OrganizationDto.cs
@@ -0,0 +1,14 @@
+namespace final_assignment.DTOs
+{
+    public class OrganizationDto
+    {
+        public string Name { get; set; } = "";
+        public string Industry { get; set; } = "";
+        public string Location { get; set; } = "";
+        public string Description { get; set; } = "";
+        public string WebsiteUrl { get; set; } = "";
+        public string ContactEmail { get; set; } = "";
+        public string ContactPhone { get; set; } = "";
+        public string CompanySize { get; set; } = "";
+    }
+}
