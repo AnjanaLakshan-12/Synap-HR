deda7d98aa5cf6a5f08e8ad5953781613b7e547b
Deshapriya LGAL <216675054+AnjanaLakshan-12@users.noreply.github.com>
2026-07-22 01:07:41 +0530

    add authentication workflows and user repository implementation
    Implemented User model UserRepository[ AuthServices for BCrypt registration/login and AuthController API endpoints

---
+++ backend/final_assignment/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using final_assignment.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace final_assignment.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string PasswordHash { get; set; } = string.Empty;
+
+        public string Role { get; set; } = "Candidate";
+        public bool IsActive { get; set; } = true;
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+
+        public int? OrganizationId { get; set; }
+        public Organization? Organization { get; set; }
+
+        public int? DepartmentId { get; set; }
+        public Department? Department { get; set; }
+    }
+}
