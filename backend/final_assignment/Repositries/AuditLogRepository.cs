86f527893f992f9d45d7c37b688083ff001dec5a
Deshapriya LGAL <216675054+AnjanaLakshan-12@users.noreply.github.com>
2026-07-22 01:15:42 +0530

    introduce AuditLog model and AuditLogRepository

---
+++ backend/final_assignment/Repositries/AuditLogRepository.cs
@@ -0,0 +1,26 @@
+﻿using final_assignment.Data;
+using final_assignment.Models;
+using final_assignment.Repositries.Interfaces;
+
+namespace final_assignment.Repositries
+{
+    public class AuditLogRepository : IAuditLogRepository
+    {
+        private readonly AppDbContext _context;
+        public AuditLogRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Add(AuditLog auditLog)
+        {
+            _context.AuditLogs.Add(auditLog);
+            _context.SaveChanges();
+        }
+
+        public List<AuditLog> GetAll()
+        {
+            return _context.AuditLogs.ToList();
+        }
+    }
+}
